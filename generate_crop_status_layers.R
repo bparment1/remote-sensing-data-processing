078e7957360eacf1c349c390c2eda229ea6e33dd
Benoit Parmentier <bparmentier@sshgw02.research.sesync.org>
2018-11-21 14:30:00 -0500

    missing input parameters for crop generation

--- generate_crop_status_layers.R
+++ generate_crop_status_layers.R
@@ -3,14 +3,14 @@
 ## 
 ##
 ## DATE CREATED: 09/12/2018
-## DATE MODIFIED: 11/20/2018
+## DATE MODIFIED: 11/26/2018
 ## AUTHORS: Benoit Parmentier  
 ## Version: 2
 ## PROJECT: Agbirds
 ## ISSUE: 
 ## TO DO:
 ##
-## COMMIT: run for Cotton crop and Alabama
+## COMMIT: missing input parameters for crop generation
 ##
 
 #### Instructions:
@@ -109,7 +109,7 @@ load_obj <- function(f){
 #Benoit setup
 script_path <- "/nfs/bparmentier-data/Data/projects/agbirds-data/scripts"
 
-crop_data_processing_functions <- "processing_crop_data_processing_functions_11212018.R"
+crop_data_processing_functions <- "processing_crop_data_processing_functions_11262018.R"
 source(file.path(script_path,crop_data_processing_functions))
 
 ############################################################################
@@ -127,7 +127,7 @@ file_format <- ".tif"
 #ARGS 5:
 create_out_dir_param=TRUE #create a new ouput dir if TRUE
 #ARGS 6
-out_suffix <-"agbirds_processing_11202018" #output suffix for the files and ouptut folder
+out_suffix <-"agbirds_processing_11262018" #output suffix for the files and ouptut folder
 #ARGS 7
 num_cores <- 2 # number of cores
 #ARGS 8
@@ -264,13 +264,14 @@ legend_df_subset <- subset(legend_df,CLASS_NAME%in% common_crop_list)
 
 region_name <- state_val
 
-if(!is.nul(crop_name)){
+if(!is.null(crop_name)){
   #undebug(generate_crop_status_raster)
   
   list_out_df <- generate_crop_status_raster(crop_name,
                                       in_filename_raster,
                                       region_name,
                                       data_screened_df,
+                                      legend_df=legend_df_subset,
                                       algorithm,
                                       num_cores,
                                       file_format,
@@ -287,7 +288,7 @@ if(!is.nul(crop_name)){
                                       in_filename_raster,
                                       region_name,
                                       data_screened_df,
-                                      legend_df,
+                                      legend_df=legend_df_subset,
                                       algorithm,
                                       num_cores,
                                       file_format,
