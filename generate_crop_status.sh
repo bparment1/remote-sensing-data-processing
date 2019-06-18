ecd752261f7fcc6b430ffd87456739e0c904265f
Benoit Parmentier <bparmentier@sshgw02.research.sesync.org>
2019-06-18 14:30:00 -0400

    selecting specific index matching states for slurm job array

--- generate_crop_status.sh
+++ generate_crop_status.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=slurm_test_crop_status        # Job name
 #SBATCH --mail-type=ALL                          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=bparmentier@sesync.org       # Where to send mail
-#SBATCH	--array=1-4                              # job array, four jobs corresponding to four tiles in Alaska
+#SBATCH	--array=1,15,18,42                              # job array, four jobs corresponding to four tiles in Alaska
 #SBATCH --ntasks=1                               # each array task gets a single CPU core
 #SBATCH --time=00:50:00                          # Time limit hrs:min:sec
 #SBATCH --output=console_%A_%a.out               # Standard output from console
@@ -13,4 +13,4 @@ pwd; hostname; date
  
 echo "Running crop status generation on a single CPU core"
  
-Rscript --vanilla /nfs/bparmentier-data/Data/projects/agbirds-data/scripts/generate_crop_status_layers_06122019c.R
+Rscript --vanilla /nfs/bparmentier-data/Data/projects/agbirds-data/scripts/generate_crop_status_layers_06192019.R
